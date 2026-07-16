778900b9cec2baf17c372d3c049fcc277caf2dca
mhayvhis34 <mhayvhis34@gmail.com>
2026-07-16 21:39:20 +0000

    Play guessing game while avoding errors

--- number_game.py
+++ number_game.py
@@ -9,4 +9,24 @@ def generate_secret_number():
 secret_number = generate_secret_number()
 print(secret_number)
 
+
+def play_game():
+    # Ask the player for a guess
+    secret_number = generate_secret_number()
+    attempts = 0
+
+    while True:
+        try:
+            guess = int(input("Enter your guess (1-100): "))
+            attempts += 1
+
+            if guess > secret_number:
+                print("Too high! Try again.")
+            elif guess < secret_number:
+                print("Too low! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Please enter a valid whole number.")
     
